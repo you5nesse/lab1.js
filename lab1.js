85d624a969953094f6c3d149f56ae37e1d64af40
youn5esse <youn17esse@gmail.com>
2026-03-08 17:41:31 +0100

    first commit

---
+++ lab1.js
@@ -0,0 +1,17 @@
+var petDog = 'Rex';
+var petCat = 'Pepper';
+
+console.log(petDog);
+console.log(petCat);
+
+console.log("Le nom de mon chien est : " + petDog);
+console.log("Le nom de mon chat est : " + petCat);
+
+var catSound = "ronron";
+var dogSound = "ouaf";
+
+console.log(petDog, "dit", dogSound);
+console.log(petCat, "dit", catSound);
+
+catSound = "miaou";
+console.log(petCat, "dit maintenant", catSound);
